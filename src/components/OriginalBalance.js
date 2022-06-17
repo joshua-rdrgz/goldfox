405372d6bb1d0130004038e1793be6db2d28c079
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-06-17 17:08:36 -0500

    add OriginalBalance component styling

--- src/components/OriginalBalance.js
+++ src/components/OriginalBalance.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+
+import classes from "../styles/layout/OriginalBalance.module.scss";
 
 function OriginalBalance() {
   return (
-    <section>
+    <section className={classes.originalBalance}>
       <label htmlFor="original-balance">
-        <h2>Original Balance</h2>
+        <h2 className={classes.originalBalance__title}>Original Balance</h2>
       </label>
-      <input type="text" name="original-balance" id="original-balance" placeholder='$1,234'/>
-      <span>Beginning Amount: $1,234</span>
+      <input
+        className={classes.originalBalance__input}
+        type="text"
+        name="original-balance"
+        id="original-balance"
+        placeholder="$1,234"
+        size="10"
+      />
+      <span className={classes.originalBalance__prevNumber}>
+        Beginning Amount: $1,234
+      </span>
     </section>
-  )
+  );
 }
 
-export default OriginalBalance
+export default OriginalBalance;
