3c0e509c1219aa265055cc9a87b89e99aec3091e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-10-02 03:45:48 -0500

    Add ability to display income/expense from store.

--- src/components/budget/income/IncomeItems.tsx
+++ src/components/budget/income/IncomeItems.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useAppSelector } from "../../../store/store-hooks";
+
 import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
 
@@ -7,22 +9,26 @@ import Number from "../../utilities/Number";
 import classes from "../../../styles/layout/budget/incexpitems.module.scss";
 
 const IncomeItems = () => {
+  const incomeItems = useAppSelector((state) => state.budget.incomeItems);
+
   return (
     <div className={classes["inc-exp__container"]}>
-      <div className={classes["inc-exp__item"]}>
-        <h5 className={classes["inc-exp__title"]}>Item</h5>
-        <p className={classes["inc-exp__category"]}>Category</p>
-        <Number className={classes["inc-exp__amount"]}>$567</Number>
-        <FaEdit className={classes["inc-exp__icon"]} />
-        <AiFillDelete className={classes["inc-exp__icon"]} />
-      </div>
-      <div className={classes["inc-exp__item"]}>
-        <h5 className={classes["inc-exp__title"]}>Item</h5>
-        <p className={classes["inc-exp__category"]}>Category</p>
-        <Number className={classes["inc-exp__amount"]}>$567</Number>
-        <FaEdit className={classes["inc-exp__icon"]} />
-        <AiFillDelete className={classes["inc-exp__icon"]} />
-      </div>
+      {incomeItems.length === 0 && (
+        <h4>Please Enter An Income Item!</h4>
+      )}
+      {incomeItems.map((item, itemIndex) => {
+        return (
+          <div className={classes["inc-exp__item"]} key={`income-item-${itemIndex + 1}`}>
+            <h5 className={classes["inc-exp__title"]}>{item.item}</h5>
+            <p className={classes["inc-exp__category"]}>{item.category}</p>
+            <Number className={classes["inc-exp__amount"]}>
+              {`$${item.amount}`}
+            </Number>
+            <FaEdit className={classes["inc-exp__icon"]} />
+            <AiFillDelete className={classes["inc-exp__icon"]} />
+          </div>
+        );
+      })}
     </div>
   );
 };
