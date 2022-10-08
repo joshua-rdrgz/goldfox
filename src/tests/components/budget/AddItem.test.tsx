6d5bf3de883b25417a015b7722038448687d9e80
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-10-08 18:55:33 -0500

    Adjust AddItem component tests
    Exclude actually adding items -- that test will belong in the Items component tests, as it technically tests both and is more relevant to the Items component.

--- src/tests/components/budget/AddItem.test.tsx
+++ src/tests/components/budget/AddItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, getElement, getInputField } from "../../utilities/test-utils";
+import { render, getElement } from "../../utilities/test-utils";
 import userEvent from "@testing-library/user-event";
 import Category from "../../../components/budget/Category";
 
@@ -7,11 +7,6 @@ describe("AddItem Income component", () => {
     render(<Category type="income" />);
     errorChecker("Income");
   });
-
-  test(`will push item to the store's incomeItems component upon successful entry of item`, () => {
-    render(<Category type="income" />);
-    addItemChecker("Income");
-  });
 });
 
 describe("AddItem Expense component", () => {
@@ -19,48 +14,13 @@ describe("AddItem Expense component", () => {
     render(<Category type="expense" />);
     errorChecker("Expense");
   });
-
-  test(`will push item to the store's expenseItems component upon successful entry of item`, () => {
-    render(<Category type="expense" />);
-    addItemChecker("Expense");
-  });
 });
 
-function addItemChecker (capitalizedType: string) {
-  // captures input elements
-  const itemInput = getInputField("Item");
-  const categoryInput = getInputField("Category...");
-  const amountInput = getInputField("$1,234");
-
-  // creates an item to display
-  userEvent.type(itemInput, "item");
-  userEvent.type(categoryInput, "category");
-  userEvent.type(amountInput, "1234");
+function errorChecker(capitalizedType: string) {
   userEvent.click(getElement(`Submit ${capitalizedType}`));
 
-  // checks for empty values in input fields
-  expect(itemInput).toHaveValue("");
-  expect(categoryInput).toHaveValue("");
-  expect(amountInput).toHaveValue(null);
-
-  // checks for existence of created item above.
-  expect(getElement("item")).toBeInTheDocument();
-  expect(getElement("category")).toBeInTheDocument();
-  expect(getElement("$1234")).toBeInTheDocument();
-};
-
-function errorChecker (capitalizedType: string) {
-  userEvent.click(getElement(`Submit ${capitalizedType}`));
-
-  // overall form error message
-  expect(getElement("Please enter the following fields:")).toBeInTheDocument();
-
-  // item error message
-  expect(getElement(`Please enter the "Item".`)).toBeInTheDocument();
-
-  // category error message
-  expect(getElement(`Please enter the "Category".`)).toBeInTheDocument();
-
-  // amount error message
-  expect(getElement(`Please enter the "Amount".`)).toBeInTheDocument();
-};
+  expect(getElement("Please enter the following fields:")).toBeInTheDocument(); // Overall form
+  expect(getElement(`Please enter the "Item".`)).toBeInTheDocument(); // Item input
+  expect(getElement(`Please enter the "Category".`)).toBeInTheDocument(); // Category input
+  expect(getElement(`Please enter the "Amount".`)).toBeInTheDocument(); // Amount input
+}
