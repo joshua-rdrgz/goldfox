78f0359ff9dc11fe8cbc49786600909b8c23b573
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-04-11 11:18:32 -0500

    update createAndSendToken() to send cookie.

--- server/src/controllers/controllerUtils.ts
+++ server/src/controllers/controllerUtils.ts
@@ -1,5 +1,5 @@
 import { promisify } from 'util';
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { UserDoc } from '@goldfoxtypes/userTypes';
 import { ISuccessfulResponseAuth } from '@goldfoxtypes/authTypes';
@@ -21,8 +21,24 @@ export const createAndSendToken = (
   res: Response,
   sendUser = false
 ) => {
+  // 1) CREATE TOKEN
   const token = signToken(user._id.toString());
 
+  // 2) CONFIGURE COOKIE
+  const ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
+  const cookieOptions: CookieOptions = {
+    expires: new Date(Date.now() + Number(process.env.JWT_COOKIE_EXPIRATION) * ONE_DAY_IN_MILLIS),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // 3) SEND COOKIE TO CLIENT
+  res.cookie('jwt', token, cookieOptions);
+
+  // 4) REMOVE PASSWORD FROM OUTPUT
+  user.password = undefined;
+
+  // 5) SEND RESPONSE TO CLIENT
   const json: ISuccessfulResponseAuth = {
     status: 'success',
     token,
