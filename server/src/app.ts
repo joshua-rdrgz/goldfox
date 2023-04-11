6007786a2b7a7f7adde2b413760154dfd0e427c4
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-04-11 11:22:01 -0500

    Implement Rate Limiting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import userRouter from '@routes/userRoutes';
 import AppError from './errors/apiError';
 import globalErrorHandler from './errors/errorController';
@@ -10,6 +11,14 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.use(
+  '/api',
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour.',
+  })
+);
 app.use(express.json());
 
 // ROUTE MOUNTING
